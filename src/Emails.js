16eed2f587900561b7c05443d198333e424dde4d
akibria12 <ahkibria@hotmail.co.uk>
2022-11-09 17:06:01 +0000

    email done

--- src/Emails.js
+++ src/Emails.js
@@ -1,36 +1,52 @@
-function Emails() {
+import Email from "./Email";
 
-    return (
-        <main className="emails">
-            <ul>
-                {filteredEmails.map((email, index) => (
-                    <li
-                    key={index}
-                    className={`email ${email.read ? 'read' : 'unread'}`}
-                    >
-                    <div className="select">
-                        <input
-                        className="select-checkbox"
-                        type="checkbox"
-                        checked={email.read}
-                        onChange={() => toggleRead(email)}
-                        />
-                    </div>
-                    <div className="star">
-                        <input
-                        className="star-checkbox"
-                        type="checkbox"
-                        checked={email.starred}
-                        onChange={() => toggleStar(email)}
-                        />
-                    </div>
-                    <div className="sender">{email.sender}</div>
-                    <div className="title">{email.title}</div>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    )
+function Emails(props) {
+  const getReadEmails = (emails) => emails.filter((email) => !email.read);
+
+  const getStarredEmails = (emails) => emails.filter((email) => email.starred);
+
+  // emails prop, setEmails
+  const toggleStar = (targetEmail) => {
+    const updatedEmails = (emails) =>
+      emails.map((email) =>
+        email.id === targetEmail.id
+          ? { ...email, starred: !email.starred }
+          : email
+      );
+    props.setEmails(updatedEmails);
+  };
+
+  const toggleRead = (targetEmail) => {
+    const updatedEmails = (emails) =>
+      emails.map((email) =>
+        email.id === targetEmail.id ? { ...email, read: !email.read } : email
+      );
+    props.setEmails(updatedEmails);
+  };
+
+  // Condition
+  let filteredEmails = props.emails;
+
+  if (hideRead) filteredEmails = getReadEmails(filteredEmails);
+
+  if (currentTab === "starred") {
+    filteredEmails = getStarredEmails(filteredEmails);
+  }
+  // End of Condition
+
+  return (
+    <main className="emails">
+      <ul>
+        {filteredEmails.map((email, index) => (
+          <Email
+            email={email}
+            toggleRead={toggleRead}
+            toggleStar={toggleStar}
+          />
+        ))}
+      </ul>
+    </main>
+  );
 }
 
-export default Emails
+export default Emails;
--- src/app.js
+++ src/app.js
@@ -7,44 +7,12 @@ import Emails from './Emails'
 
 import './styles/app.css'
 
-const getReadEmails = emails => emails.filter(email => !email.read)
 
-const getStarredEmails = emails => emails.filter(email => email.starred)
 
 function App() {
   const [emails, setEmails] = useState(initialEmails)
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
-
- 
-
-  const toggleStar = targetEmail => {
-    const updatedEmails = emails =>
-      emails.map(email =>
-        email.id === targetEmail.id
-          ? { ...email, starred: !email.starred }
-          : email
-      )
-    setEmails(updatedEmails)
-  }
-
-  const toggleRead = targetEmail => {
-    const updatedEmails = emails =>
-      emails.map(email =>
-        email.id === targetEmail.id ? { ...email, read: !email.read } : email
-      )
-    setEmails(updatedEmails)
-  }
-
-  // Condition
-  let filteredEmails = emails
-
-  if (hideRead) filteredEmails = getReadEmails(filteredEmails)
-
-  if (currentTab === 'starred') {
-    filteredEmails = getStarredEmails(filteredEmails)
-  }
-  // End of Condition
 
 
   return (
@@ -60,7 +28,9 @@ function App() {
         hideRead={hideRead}
       />
       
-      <Emails />
+      <Emails 
+        emails={emails}
+        setEmails={setEmails}/>
     </div>
   )
 }
