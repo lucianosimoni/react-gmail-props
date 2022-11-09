bbaf9e84003bc09b128c1a373d467e74b6563554
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-09 16:34:35 +0000

    App sliced in 3 components
    - Header (Completed.)
    - LeftMenu (Completed.)
    - Emails
    
    Emails need to be updated with respective variables and function if needed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 
 import initialEmails from './data/emails'
+import Header from './Header'
+import LeftMenu from './LeftMenu'
+import Emails from './Emails'
 
 import './styles/app.css'
 
@@ -13,8 +16,7 @@ function App() {
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
 
-  const unreadEmails = emails.filter(email => !email.read)
-  const starredEmails = emails.filter(email => email.starred)
+ 
 
   const toggleStar = targetEmail => {
     const updatedEmails = emails =>
@@ -34,88 +36,31 @@ function App() {
     setEmails(updatedEmails)
   }
 
+  // Condition
   let filteredEmails = emails
 
   if (hideRead) filteredEmails = getReadEmails(filteredEmails)
 
-  if (currentTab === 'starred')
+  if (currentTab === 'starred') {
     filteredEmails = getStarredEmails(filteredEmails)
+  }
+  // End of Condition
+
 
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
-      <nav className="left-menu">
-        <ul className="inbox-list">
-          <li
-            className={`item ${currentTab === 'inbox' ? 'active' : ''}`}
-            onClick={() => setCurrentTab('inbox')}
-          >
-            <span className="label">Inbox</span>
-            <span className="count">{unreadEmails.length}</span>
-          </li>
-          <li
-            className={`item ${currentTab === 'starred' ? 'active' : ''}`}
-            onClick={() => setCurrentTab('starred')}
-          >
-            <span className="label">Starred</span>
-            <span className="count">{starredEmails.length}</span>
-          </li>
-
-          <li className="item toggle">
-            <label htmlFor="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              checked={hideRead}
-              onChange={e => setHideRead(e.target.checked)}
-            />
-          </li>
-        </ul>
-      </nav>
-      <main className="emails">
-        <ul>
-          {filteredEmails.map((email, index) => (
-            <li
-              key={index}
-              className={`email ${email.read ? 'read' : 'unread'}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <Header />
+      
+      {/* setCurrentTab currentTab setHideRead HideRead */}
+      <LeftMenu 
+        emails={emails}
+        setCurrentTab={setCurrentTab}
+        setHideRead={setHideRead}
+        currentTab={currentTab}
+        hideRead={hideRead}
+      />
+      
+      <Emails />
     </div>
   )
 }
